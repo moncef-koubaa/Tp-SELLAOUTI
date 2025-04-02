f852e36ea88f9ddf122cfc55fbaf2b9a01d82cbd
Moncef Koubaa <moncefkoubaa26@gmail.com>
2025-04-02 08:42:54 +0100

    feat : impliment cv seeding with fake data

--- tp1/src/cv/entities/cv.entity.ts
+++ tp1/src/cv/entities/cv.entity.ts
@@ -1,1 +1,18 @@
-export class Cv {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Cv {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column()
+  firstName: string;
+  @Column()
+  age: number;
+  @Column()
+  CIN: string;
+  @Column()
+  job: string;
+  @Column()
+  path: string;
+}
